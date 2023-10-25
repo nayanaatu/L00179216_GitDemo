766595b0f93a52c383fb6b4604d183f996ea90c0
L00179216 <L00179216@atu.ie>
2023-10-26 00:58:44 +0530

    testing code added

---
+++ src/dev/helloworld.py
@@ -0,0 +1,8 @@
+#/usr/bin/python3
+import sys
+
+def helloworld(out):
+    out.write("Hello world of Python\n")
+
+if __name__ == "__main__":
+    print(main())
--- src/unittest/python/helloworld_tests.py
+++ src/unittest/python/helloworld_tests.py
@@ -1,9 +1,16 @@
 from mockito import mock, verify
 import unittest
-from helloworld import helloworld
+
+import sys
+import os.path
+sys.path.append('C:\\Users\\nnagaraj\\L00179216_GitDemo\\src\\')
+print("SYS : ", sys.path)
+print("hh")
+from dev import helloworld
 
 class HelloWorldTest(unittest.TestCase):
     def test_should_issue_hello_world_message(self):
         out = mock()
-        helloworld(out)
+        helloworld.helloworld(out)
+        print("\n=========== TEST execution ========\n")
         verify(out).write("Hello world of Python\n")
