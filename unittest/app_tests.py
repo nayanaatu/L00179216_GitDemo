e5add0a623a94d63d77972a9e62e0b797797e024
L00179216 <L00179216@atu.ie>
2023-11-08 06:19:03 +0530

    test file updated

--- unittest/app_tests.py
+++ unittest/app_tests.py
@@ -29,4 +29,4 @@ class WebAppTest(unittest.TestCase):
         This is the test method to check the message of ATUapp
         """
         print("\n=========== TEST2 execution ========\n")
-        assert hello_msg() == "HELLO, WELCOME TO ATU LETTERKENNY UNIVERSITY {name}!!!"
+        assert hellow_msg() == "HELLO, WELCOME TO ATU LETTERKENNY UNIVERSITY {name}!!!"
