18196729431aaaef8482a17693dc5479ec476ae1
L00179216 <L00179216@atu.ie>
2023-11-08 06:28:19 +0530

    identation error updated

--- dev/ATUapp.py
+++ dev/ATUapp.py
@@ -2,23 +2,23 @@ from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
 
-@app.route('/<name>')
+@app.route('/hello/<name>')
 def hello_msg(name):
-	return f"HELLO, WELCOME TO ATU LETTERKENNY UNIVERSITY {name}!!!"
+    return f"HELLO, WELCOME TO ATU LETTERKENNY UNIVERSITY {name}!!!"
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-	if request.method == 'POST':
+    if request.method == 'POST':
         user = request.form['nm']
-        var = redirect(url_for(name=user))
-        print("retrun val : ", var)
+        var = redirect(url_for('hello', name=user))
+        print("return val : ", var)
         return var
-	else:
-		user = request.args.get('nm')
-		return redirect(url_for(name=user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('hello', name=user))
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
 
