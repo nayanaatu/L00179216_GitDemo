551d72da579f9a01cbce7cea51657e39f9059398
L00179216 <L00179216@atu.ie>
2023-11-08 07:26:37 +0530

    app file updated

--- dev/app.py
+++ dev/app.py
@@ -1,9 +1,14 @@
+#/usr/local/bin/python3
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_msg():
+def hellow_msg():
+    """
+    method to return hello message
+    """
     return "HELLO, WELCOME TO ATU LETTERKENNY UNIVERSITY!!!"
 
 if __name__ == "__main__":
