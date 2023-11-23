4f5a637f3ebc114710733f43f531a0cbe8680862
L00179216 <L00179216@atu.ie>
2023-11-23 07:19:37 +0530

    test method identation error addressed

--- dev/app.py
+++ dev/app.py
@@ -1,6 +1,16 @@
 #/usr/local/bin/python3
 
 from flask import Flask
+from colorama import init, Fore, Back, Style
+
+init()
+
+# all available foreground colors
+FORES = [ Fore.BLACK, Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN, Fore.WHITE ]
+# all available background colors
+BACKS = [ Back.BLACK, Back.RED, Back.GREEN, Back.YELLOW, Back.BLUE, Back.MAGENTA, Back.CYAN, Back.WHITE ]
+# brightness values
+BRIGHTNESS = [ Style.DIM, Style.NORMAL, Style.BRIGHT]
 
 app = Flask(__name__)
 
@@ -16,6 +26,16 @@ def student_postgraduate(student_label_number):
         return f"Welcome Postgraduate Students"
     else:
         return f"Welcome Graduate Students"
+
+@app.route("/")
+def print_with_color(s, color=Fore.WHITE, brightness=Style.NORMAL, **kwargs):
+    """Utility function wrapping the regular `print()` function 
+    but with colors and brightness"""
+    print(f"{brightness}{color}{s}{Style.RESET_ALL}", **kwargs)
+
+for fore in FORES:
+    for brightness in BRIGHTNESS:
+        return print_with_color("HELLO, WELCOME TO ATU LETTERKENNY UNIVERSITY!!!", color=fore, brightness=brightness)
 
 
 if __name__ == "__main__":
