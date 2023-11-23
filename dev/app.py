0bc20c3ff7dd977deb70e45962c20bc042144bb7
L00179216 <L00179216@atu.ie>
2023-11-23 08:40:32 +0530

    html code added

--- dev/app.py
+++ dev/app.py
@@ -9,8 +9,16 @@ def hellow_msg():
     """
     Method to return hello message
     """
-    strng = "\033[1;32m HELLO, WELCOME TO ATU LETTERKENNY UNIVERSITY!!! \n"
-    return strng 
+    #strng = "HELLO, WELCOME TO ATU LETTERKENNY UNIVERSITY!!!"
+
+    my_html =  '''
+    <body style="background-color: #e1eb34;">
+        <h1 style="color:  #006400;">"HELLO, WELCOME TO ATU LETTERKENNY UNIVERSITY!!!"</h1>
+    </body>
+    '''
+    print(my_html)
+    return my_html
+
 
 def student_postgraduate(student_label_number):
     """
